a953213e11ba005208cdaa2da9541687605c3a42
Backu68 <backu68@gmail.com>
2026-09-06 10:50:08 -0600

    Add SSD1306 dryer display backend

---
+++ src/dryer/SSD1306DryerDisplay.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+
+#include "DryerDisplayI.h"
+
+class SSD1306DryerDisplay : public DryerDisplayI {
+public:
+    SSD1306DryerDisplay(uint8_t sdaPin, uint8_t sclPin, uint8_t address = 0x3C);
+
+    bool begin() override;
+    bool isAvailable() const override;
+    void render(const DryerRuntimeStatus& status) override;
+    void getDisplayInfo(char* buf, size_t len) const override;
+
+private:
+    void drawStationLine(
+        int y,
+        const char* name,
+        DryerStation station,
+        const DryerStationView& state,
+        DryerStation selectedStation
+    );
+
+    static String stationLabel(const DryerStationView& state);
+    static String sessionCompact(const DryerStationView& state);
+
+    uint8_t sdaPin_;
+    uint8_t sclPin_;
+    uint8_t address_;
+    bool available_ = false;
+    Adafruit_SSD1306 display_;
+};
