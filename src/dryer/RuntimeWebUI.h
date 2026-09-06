531fe7236eb9304d77e8f48d313d35f8ae573255
Backu68 <backu68@gmail.com>
2026-09-06 09:44:08 -0600

    Add dryer runtime web UI interface

---
+++ src/dryer/RuntimeWebUI.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Arduino.h>
+#include <WebServer.h>
+
+#include "SpoolmanClient.h"
+
+struct RuntimeStationView {
+    bool occupied = false;
+    String uid;
+    DryerSpoolInfo spool;
+    String lookupError;
+};
+
+struct RuntimeStatus {
+    float chamberTempC = 0.0f;
+    bool temperatureValid = false;
+    bool wifiConnected = false;
+    bool spoolmanConfigured = false;
+    bool nfcAvailable = false;
+    bool topSelected = true;
+    RuntimeStationView top;
+    RuntimeStationView bottom;
+};
+
+class RuntimeWebUI {
+public:
+    using StatusProvider = RuntimeStatus (*)();
+    using ClearHandler = void (*)(bool topStation);
+
+    bool begin(StatusProvider statusProvider, ClearHandler clearHandler);
+    void loop();
+    bool isActive() const;
+
+private:
+    void handleRoot();
+    void handleStatus();
+    void handleClear();
+
+    static String htmlEscape(const String& value);
+
+    WebServer server_{80};
+    StatusProvider statusProvider_ = nullptr;
+    ClearHandler clearHandler_ = nullptr;
+    bool active_ = false;
+};
